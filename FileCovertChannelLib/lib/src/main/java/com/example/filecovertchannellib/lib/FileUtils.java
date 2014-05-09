862b4e03f7bde78cc6903ea62f60bfbd9c1170b8
theard <TimHeard@hotmail.com>
2014-05-09 15:51:29 -0400

    Started implementing basic functionality and added a library project for common functionality.

---
+++ FileCovertChannelLib/lib/src/main/java/com/example/filecovertchannellib/lib/FileUtils.java
@@ -0,0 +1,63 @@
+package com.example.filecovertchannellib.lib;
+
+import android.support.v4.util.TimeUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Tim on 5/9/2014.
+ */
+public class FileUtils
+{
+    /**
+     * Updates the timestamp of the given file to the current time.
+     * Taken from http://www.intransitione.com/blog/touch-a-file-on-android/
+     */
+    public static void touch(File file) throws IOException {
+        if(!file.exists())
+        {
+            File parent = file.getParentFile();
+            if(parent != null)
+                if(!parent.exists())
+                    if(!parent.mkdirs())
+                        throw new IOException("Cannot create parent directories for file: " + file);
+
+            file.createNewFile();
+        }
+
+        boolean success = file.setLastModified(System.currentTimeMillis());
+        if (!success)
+            throw new IOException("Unable to set the last modification time for " + file);
+    }
+
+    /**
+     * Waits until the given syncFile is updated ("touched") by another process. The caller
+     * will sleep for sleepInterval milliseconds in between each check of the file.
+     *
+     * @param syncFile          The file being monitored
+     * @param lastUpdateTime    The time of the last known update to the file
+     * @param sleepInterval     How long to sleep between checks of the file's last update time
+     *
+     * @return                  An updated last-modified-time value
+     *
+     * @throws InterruptedException If the calling thread is interrupted while asleep.
+     */
+    public static long waitOn(File syncFile, long lastUpdateTime, long sleepInterval)
+        throws InterruptedException
+    {
+        long updatedTime;
+        while(true)
+        {
+            if(syncFile.lastModified() > lastUpdateTime)
+            {
+                updatedTime = syncFile.lastModified();
+                break;
+            }
+
+            Thread.sleep(sleepInterval);
+        }
+
+        return updatedTime;
+    }
+}
--- FilePipelineCovertChannel/app/src/main/java/com/example/filepipelinecovertchannel/app/MainActivity.java
+++ FilePipelineCovertChannel/app/src/main/java/com/example/filepipelinecovertchannel/app/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.filepipelinecovertchannel.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    private EditText messageEntry;
+    private TextView outputDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageEntry = (EditText) findViewById(R.id.messageEntry);
+        outputDisplay = (TextView) findViewById(R.id.statusMessageDisplay);
+    }
+
+    public void sendMessageClick(View v)
+    {
+        String message = messageEntry.getText().toString();
+
+        if(message == null || message.length() == 0)
+        {
+            outputDisplay.setText("Cannot send message; message length is zero.");
+        }
+        else
+        {
+            outputDisplay.setText("Message '" + message + "' was sent");
+        }
     }
 }

d6d7af143977fa0b4ed9a7ce33a48df8da85dd47
agustin2489 <agustin2489@gmail.com>
2014-05-10 20:25:15 -0400

    Added logic to the Writer
    Added logic for breaking message down, waiting for writeready flag,
    encoding to carrier file, writing a readready flag

--- FilePipelineCovertChannel/app/src/main/java/com/example/filepipelinecovertchannel/app/MainActivity.java
+++ FilePipelineCovertChannel/app/src/main/java/com/example/filepipelinecovertchannel/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.filepipelinecovertchannel.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -8,10 +9,16 @@ import android.widget.TextView;
 
 import com.example.filecovertchannellib.lib.FileUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class MainActivity extends Activity
 {
     private EditText messageEntry;
     private TextView outputDisplay;
+    private File writeready = new File("writeready");
+    private FileOutputStream carrier;
+    private FileOutputStream readready;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,21 @@ public class MainActivity extends Activity
         }
         else
         {
+            //break string into chars
+            for(int i = 0; i < message.length(); i++) {
+                char c = message.charAt(i);
+                //check for writeready flag
+                if( writeready.exists() ) {
+                    //encode into carrier file
+                    carrier = openFileOutput("carrier", Context.MODE_WORLD_READABLE);
+                    carrier.write(c);
+                    carrier.close();
+                }
+                readready = openFileOutput("readready", Context.MODE_WORLD_READABLE);
+                readready.write(1);
+                readready.close();
+            }
+
             outputDisplay.setText("Message '" + message + "' was sent");
         }
     }
